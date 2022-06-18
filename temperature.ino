ef6e4459b0c8e9007e0b49a5b55b7bbdc000de93
gooseBot <hipystix@gmail.com>
2022-06-17 19:20:24 -0700

    Describe LM35 conditioning circuit

--- temperature.ino
+++ temperature.ino
@@ -1,5 +1,9 @@
 
-
+// Build capacitor and resistor circut as in this link
+// https://bestengineeringprojects.com/lm35-how-to-get-stable-temperature-reading/
+// otherwise output is not stable and varies up to 6C!
+// 1k ohm across output and ground to sensor
+// 10 micro farad across vcc and ground to sensor
 void readTemperature() {
   const int temperaturePin = A2;
   int reading = analogRead(temperaturePin);
